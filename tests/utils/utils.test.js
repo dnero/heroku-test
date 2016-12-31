ab1aa8c70a2b3a2cffe2bcd114971cf6b1772a6f
D'ontreye Nero <dontreyenero@gmail.com>
2016-12-31 18:58:37 -0500

    Add expect assertion library

--- tests/utils/utils.test.js
+++ tests/utils/utils.test.js
@@ -1,18 +1,39 @@
 const utils = require('./utils');
+const expect = require('expect');
+
 
 it('should add two numbers', () => {
 	let res = utils.add(33, 11);
 	
-	if (res !== 44) {
-		throw new Error(`Expected 44, but got ${res}.`);
-	}
+	expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
 	
-	let res = utils.square(2);
+	let res = utils.square(4);
 	
-	if(res !== 4) {
-		throw new Error(`Expected 4, but got ${res}.`);
-	}
+	expect(res).toBe(16).toBeA('number');
 });
+
+it('should verify that first and last names are set', () => {
+	let user = {location: 'ATL', age: 34};
+	let res = utils.setName(user, 'Deon Nero');
+	expect(res).toInclude({
+		firstName: 'Deon',
+		lastName: 'Nero'
+	});
+});
+
+// it('should expect some values', () => {
+// 	//expect(12).toNotBe(1);
+// 	//expect({name: 'Deon'}).toEqual({name: 'Deon'});
+// 	expect([2, 3, 4]).toInclude(2);
+// 	expect({
+// 		name: 'Deon',
+// 		age: 34,
+// 		location: 'ATL'
+// 	}).toInclude({
+// 		age: 34,
+// 		name: 'Deon'
+// 	})
+// });
